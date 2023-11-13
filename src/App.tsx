0086ab3d7a03256daf92f6ebd7064dbec2b25393
WaydinJochem <jochem7405@gmail.com>
2023-11-13 15:30:53 +0200

    Still need to work on Responsive view

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import "./App.css";
 import Navbar from "./Components/Navbar";
-import Navigation from "./Components/Navigation";
 import "bootstrap/dist/css/bootstrap.css";
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import imagePath from './assets/react.svg'
@@ -9,8 +8,7 @@ function App() {
   const items = ["Home", "About", "Projects", "Skills","Contact"]
   return (
     <>
-    <Navigation/>
-      {/* <Navbar brandName="Menu" imageScrPath={imagePath} navItems={items}/> */}
+      <Navbar brandName="Menu" imageScrPath={imagePath} navItems={items}/>
     </>
   );
 }
--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -10,32 +10,30 @@ const Navbar = ({ brandName, imageScrPath, navItems }: NavbarProps) => {
   const [selectedIndex, setselectedIndex] = useState(-1);
   return (
     <>
-        <nav className="navbar navbar-light bg-black fw-bold navbar-expand-lg w-sm-25">
-          <div className="container-fluid">
-            <a className="navbar-brand d-none d-md-block" href="#">
-              <img src={imageScrPath} width="60" height="60" alt="" />
-              <span className="fw-bolder fs-4 text-white">{brandName}</span>
-            </a>
-            {/* Nav Button */}
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            {/* Nav Button End */}
-            {/* Nav items */}
-            <div
-              className="collapse navbar-collapse"
-              id="navbarSupportedContent"
-            >
-              <ul className="navbar-nav ms-auto d-flex justify-content-between align-items-center me-5-md py-2 flex-column flex-lg-row">
-                {navItems.map((items, index) => (
+      <nav className="navbar navbar-light bg-black fw-bold navbar-expand-lg w-sm-25">
+        <div className="container-fluid">
+          <a className="navbar-brand d-none d-md-block" href="#">
+            <img src={imageScrPath} width="60" height="60" alt="" />
+            <span className="fw-bolder fs-4 text-white">{brandName}</span>
+          </a>
+          {/* Nav Button */}
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          {/* Nav Button End */}
+          {/* Nav items */}
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ms-auto d-flex justify-content-between align-items-center me-5-md py-2 flex-column flex-lg-row">
+              {navItems.map((items, index) => (
+                <>
                   <li
                     key={items}
                     className={`nav-item ${
@@ -47,23 +45,29 @@ const Navbar = ({ brandName, imageScrPath, navItems }: NavbarProps) => {
                       {items}
                     </a>
                   </li>
-                ))}
-                <li className="nav-item">
-                  <a className="nav-link text-decoration-none" href="#" id="dwn">
-                    Download CV
-                  </a>
+                </>
+              ))}
+              <li className="nav-item">
+                <a className="nav-link text-decoration-none" href="#" id="dwn">
+                  Download CV
+                </a>
+                <div
+                  className="d-flex
+                  justify-content-center"
+                >
                   <img src={imageScrPath} alt="" />
-                </li>
-              </ul>
-            </div>
-            {/* Nav Items End */}
-
-            <a className="navbar-brand d-md-none mx-auto" href="#">
-              <img src={imageScrPath} width="60" height="60" alt="" />
-              <span className="fw-bolder fs-4 text-white">Navigation</span>
-            </a>
+                </div>
+              </li>
+            </ul>
           </div>
-        </nav>
+          {/* Nav Items End */}
+
+          <a className="navbar-brand d-md-none mx-auto" href="#">
+            <img src={imageScrPath} width="60" height="60" alt="" />
+            <span className="fw-bolder fs-4 text-white">Navigation</span>
+          </a>
+        </div>
+      </nav>
     </>
   );
 };
