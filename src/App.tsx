62d622821a1aa59e2e2af176b91c36371acf603c
WaydinJochem <jochem7405@gmail.com>
2023-11-20 16:06:38 +0200

    Remove NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import "./App.css";
-import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
-import imagePath from "./assets/icons8-download-80.png"
 import scr from "./assets/icons8-location-50.png"
+
 
 function App() {
   const items = ["Home", "About", "Projects", "Skills","Contact"]
   return (
     <>
-      <Navbar brandName="Menu" imageScrPath={imagePath} navItems={items}/>
       <Footer imageSrc={scr}/>
     </>
   );
