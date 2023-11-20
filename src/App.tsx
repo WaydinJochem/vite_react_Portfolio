2fb2827b0c0d31a35f6f9a68a59c9ecf95c02329
WaydinJochem <jochem7405@gmail.com>
2023-11-20 12:25:26 +0200

    Cleaning Up before Merge

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,13 @@
 import "./App.css";
-import Navbar from "./Components/Navbar";
-import Footer from "./Components/Footer";
 import SkillComp from "./Components/SkillComp";
 import imagePath from "./assets/icons8-download-80.png";
 
 function App() {
-  const items = ["Home", "About", "Projects", "Skills", "Contact"];
   return (
     <>
-      <Navbar brandName="Menu" imageScrPath={imagePath} navItems={items} />
       <main>
        <SkillComp/>
       </main>
-      <Footer />
     </>
   );
 }
