072833d258bca283685c9728509f7852d954c592
WaydinJochem <jochem7405@gmail.com>
2023-11-21 08:36:16 +0200

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import "./App.css";
 import Footer from "./Components/Footer";
-import scr from "./assets/icons8-location-50.png"
 
 
 function App() {
-  const items = ["Home", "About", "Projects", "Skills","Contact"]
   return (
     <>
-      <Footer imageSrc={scr}/>
+      <Footer/>
     </>
   );
 }
