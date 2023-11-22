fa332e50aecef639c181306c5a826fd2fe5999c1
WaydinJochem <jochem7405@gmail.com>
2023-11-22 09:41:20 +0200

    Still far from complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
-import "./App.css";
-import Navbar from "./Components/Navbar";
-import "bootstrap/dist/css/bootstrap.css";
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import imagePath from './assets/react.svg'
+import "./index.css";
+import ContactPage from "./Components/ContactPage";
+
 
 function App() {
-  const items = ["Home", "About", "Projects", "Skills","Contact"]
   return (
     <>
-      <Navbar brandName="Menu" imageScrPath={imagePath} navItems={items}/>
+      <ContactPage/>
     </>
   );
 }
---
+++ src/Components/ContactPage.tsx
@@ -0,0 +1,12 @@
+import Form from './Contact/Form'
+import "../Style/Contact.css"
+
+const ContactPage = () => {
+  return (
+    <div className="contact">
+    <Form/>
+    </div>
+  )
+}
+
+export default ContactPage
